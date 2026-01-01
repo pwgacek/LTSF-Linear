34a10a5b033db421ee87e2da4aa297fccd0621d0
Paweł Gacek <80721230+pwgacek@users.noreply.github.com>
2026-01-01 17:30:51 +0100

    Conditionally remove target feature from columns

--- FEDformer/data_provider/data_loader.py
+++ FEDformer/data_provider/data_loader.py
@@ -227,9 +227,10 @@ class Dataset_Custom(Dataset):
         df_raw.columns: ['date', ...(other features), target feature]
         '''
         cols = list(df_raw.columns)
-        cols.remove(self.target)
+        
+        if self.features == 'S':
+            cols.remove(self.target)
         cols.remove('date')
-        df_raw = df_raw[['date'] + cols + [self.target]]
         # print(cols)
         num_train = int(len(df_raw) * 0.7)
         num_test = int(len(df_raw) * 0.2)
